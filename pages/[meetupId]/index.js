2258e45360f236faf6c46c851e4b1fd6799c6171
rsumeetprdxn <sumeetr@prdxn.com>
2021-07-03 00:58:55 +0530

    new meetup & single meetup page added

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,6 @@
+export default function singleMeetup() {
+    return (
+      <h1>Single Meetup Page</h1>
+    )
+  }
+  
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,6 @@
+export default function newMeetup() {
+    return (
+      <h1>New Meetup Page</h1>
+    )
+  }
+  
